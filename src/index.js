1a7d58021bf593f99f74967ae1c4c79d8e989159
Vũ Đạt <74701328+dat911zz@users.noreply.github.com>
2023-03-28 08:19:46 +0000

    update

--- src/index.js
+++ src/index.js
@@ -1,26 +1,27 @@
 const Zing = require('../modules/ZingMp3');
 
-// ./src/index.js 
-console.log('Hello there!');
-
 // importing the dependencies
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const helmet = require('helmet');
 const morgan = require('morgan');
 
 // defining the Express app
 const app = express();
 
-const ads = [
+const homeTitle = [
   { title: 'API to crawl songs from ZingMp3' },
   { author: 'dat911zz' },
   { link_docs: 'https://zingmp3api-dvn.onrender.com/api-docs/' }
 ];
 
-// adding Helmet to enhance your Rest API's security
-app.use(helmet());
+//Logip
+app.use((req, res, next) => {
+  var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  console.log("Request ip: " + ip);
+  next();
+});
+
 
 // using bodyParser to parse JSON bodies into JS objects
 app.use(bodyParser.json());
@@ -33,76 +34,72 @@ app.use(morgan('combined'));
 
 // error handler middleware
 app.use((error, req, res, next) => {
-  console.log('in Error middleware')
+  console.log('In Error middleware: ' + req.path);
+  console.log('Respond: ' + res);
   console.error(error.stack);
-  res.status(500).send(error.message || 'Something Broke!');
+  next();
 })
 
 const swaggerUi = require('swagger-ui-express'),
   swaggerDocument = require('../swagger.json');
 
-
-try {
-  app.use(
-    '/api-docs',
-    swaggerUi.serve,
-    swaggerUi.setup(swaggerDocument)
-  );
-  // defining an endpoint to return all ads
-  app.get('/', (req, res) => {
-    res.send(ads);
-  });
-  app.get('/home', (req, res) => {
-    Zing.getDetailPlaylist('ZU9ZO7DU')
+app.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument)
+);
+app.get('/err1', (req, res) => {
+  res.status(500).send("Lỗi đang test hệ thống!");
+});
+// defining an endpoint to return all ads
+app.get('/', (req, res) => {
+  res.send(homeTitle);
+});
+app.get('/home', (req, res) => {
+  Zing.getDetailPlaylist('ZU9ZO7DU')
     .then(data => res.json(data), error => res.json(error))
     .catch(err => console.log("error:", err));
-  });
-  app.get('/getChartHome', (req, res) => {
-    Zing.getChartHome()
+});
+app.get('/getChartHome', (req, res) => {
+  Zing.getChartHome()
     .then(data => res.json(data), error => res.json(error))
     .catch(err => console.log("error:", err));
-  });
-  app.get('/top100', (req, res) => {
-    Zing.getTop100()
+});
+app.get('/top100', (req, res) => {
+  Zing.getTop100()
     .then(data => res.json(data), error => res.json(error))
     .catch(err => console.log("error:", err));
-  });
-  app.get('/getSongInfo/:id', (req, res) => {
-    Zing.getInfoMusic(req.params.id)
+});
+app.get('/getSongInfo/:id', (req, res) => {
+  Zing.getInfoMusic(req.params.id)
     .then(data => res.json(data), error => res.json(error))
     .catch(err => console.log("error:", err));
-  });
-  app.get('/getStreaming/:id', (req, res) => {
-    Zing.getStreaming(req.params.id)
+});
+app.get('/getStreaming/:id', (req, res) => {
+  Zing.getStreaming(req.params.id)
     .then(data => res.json(data), error => res.json(error))
     .catch(err => console.log("error:", err));
-  });
-  app.get('/getFullInfo/:id', (req, res) => {
-    Zing.getFullInfo(req.params.id)
+});
+app.get('/getFullInfo/:id', (req, res) => {
+  Zing.getFullInfo(req.params.id)
     .then(data => res.json(data), error => res.json(error))
     .catch(err => console.log("error:", err));
-  });
-  app.get('/getDetailPlaylist/:id', (req, res) => {
-    Zing.getDetailPlaylist(req.params.id)
+});
+app.get('/getDetailPlaylist/:id', (req, res) => {
+  Zing.getDetailPlaylist(req.params.id)
     .then(data => res.json(data), error => res.json(error))
     .catch(err => console.log("error:", err));
-  });
-  app.get('/getDetailArtist/:alias', (req, res) => {
-    Zing.getDetailArtist(req.params.alias)
+});
+app.get('/getDetailArtist/:alias', (req, res) => {
+  Zing.getDetailArtist(req.params.alias)
     .then(data => res.json(data), error => res.json(error))
     .catch(err => console.log("error:", err));
-  });
-  app.get('/search/:keyword', (req, res) => {
-    Zing.search(req.params.keyword)
+});
+app.get('/search/:keyword', (req, res) => {
+  Zing.search(req.params.keyword)
     .then(data => res.json(data), error => res.json(error))
     .catch(err => console.log("error:", err));
-  });
-
-} catch (ex) {
-  console.log('Ex: ' + ex);
-  app.response.send("error");
-}
-console.log(Zing.getDetailPlaylist('ZU9ZO7DU'));
+});
 // starting the server
 app.listen(3000, () => {
   console.log('listening on port 3000');
