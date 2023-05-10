ce90f8d7bd83fc0ff65ed5e0b5aa3413a365c93a
Vũ Đạt <74701328+dat911zz@users.noreply.github.com>
2023-05-10 14:51:20 +0000

    rollback

--- modules/ZingMp3.js
+++ modules/ZingMp3.js
@@ -9,12 +9,7 @@ const API_KEY = '88265e23d4284f25963e6eedac8fbfa3';
 const SECRET_KEY = '2aa2d1c561e809b267f3638c4a307aab';
 const VERSION = '1.6.34';
 
-const cookiePath = 'ZingMp3.json';
-
-if (!fs.existsSync(cookiePath)) fs.closeSync(fs.openSync(cookiePath, 'w'));
-
-let cookiejar = request.jar(new FileCookieStore(cookiePath));
-cookiejar._jar.enableLooseMode = false;
+let cookiejar = request.jar();
 
 request = request.defaults({
     baseUrl: URL_API,
@@ -178,6 +173,8 @@ class ZingMp3 {
 
     hashParam(path, param = '', haveParam = 0) {
         this.time = Math.floor(Date.now() / 1000);
+        // this.time = '1634406003';
+
         let strHash = `ctime=${this.time}`;
         if (haveParam === 0) strHash += param;
         const hash256 = encrypt.getHash256(strHash);
